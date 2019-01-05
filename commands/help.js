9994d6037a474e46be4e3a4fdd9e746851e19dbf
sythemedia <36555749+sythemedia@users.noreply.github.com>
2019-01-05 01:24:03 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -21,22 +21,17 @@ if(!prefix){
     message.channel.send("```" + "\n" +
                         "Type " + prefix + "cat for a random cute cat!\n" +
                         "Type " + prefix + "doggo for a random cute dog!\n" +
-                        "Type " + prefix + "hi to chat with the bot!\n" +
-                        "Type " + prefix + "ask to ask a question to chen!\n" +
                         "Type " + prefix + "vote to create a vote!\n" +
                         "Type " + prefix + "luckycolor to get a hexcolor code!\n" +
                         "Type " + prefix + "say to let the bot say for you!\n" +
                         "Type " + prefix + "embed to let the bot embed for you!\n" +
                         "Type " + prefix + "flip to flip a coin!\n" +
                         "Type " + prefix + "asciiart to get a random asciiart!\n" +
-                        "Type " + prefix + "honk to get a random honk song!\n" +
                         "Type " + prefix + "vaporwave [text] to vapor text!\n" +
                         "Type " + prefix + "encode [text] to encode text!\n" +
                         "Type " + prefix + "decode [text] to decode text!\n" +
                         "Type " + prefix + "sample to get a sample text!\n" +
                         "Type " + prefix + "code[text] to send styled codes!\n" +
-                        "Type " + prefix + "fakemusic [songname] to make a fake musicplayer!\n" +
-                        "Type " + prefix + "party to get a party website!\n" +
                         "Type " + prefix + "uselessfact to get a useless fact!\n" +
                         "Type " + prefix + "uselessweb to get a useless website!\n" +
                         "Type " + prefix + "google [stuff] to google stuffs!\n"
