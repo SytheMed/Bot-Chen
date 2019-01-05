ec3568ae24596159c0068d716b6e36d81d89a9ac
sythemedia <36555749+sythemedia@users.noreply.github.com>
2019-01-05 01:13:58 -0500

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -7,8 +7,7 @@ module.exports.noPerms = (message, perm) => {
         .setAuthor(message.author.username)
         .setTitle("Insufficient Permission")
         .setColor(colorp.red)
-        .addField("Permission needed", perm)
-        .setThumbnail(`https://78.media.tumblr.com/bae6877a04f06d34ef9464806c4a7296/tumblr_obwoxdilyJ1u349h5o1_500.gif`);
+        .addField("Permission needed", perm);
 
     message.channel.send(embed).then(msg => msg.delete(5000));
 }
@@ -19,8 +18,7 @@ module.exports.equalPerms = (message, user, perms) => {
         .setAuthor(message.author.username)
         .setColor(colorp.red)
         .setTitle("Error")
-        .addField(`${user} has perms`, perms)
-        .setThumbnail(`https://78.media.tumblr.com/bae6877a04f06d34ef9464806c4a7296/tumblr_obwoxdilyJ1u349h5o1_500.gif`);
+        .addField(`${user} has perms`, perms);
 
     message.channel.send(embed).then(msg => msg.delete(5000));
 
@@ -30,8 +28,7 @@ module.exports.botuser = (message) => {
     let embed = new Discord.RichEmbed()
         .setTitle("Error")
         .setDescription("You cannot ban a bot.")
-        .setColor(colorp.red)
-        .setThumbnail(`https://i.kym-cdn.com/photos/images/original/000/787/536/32d.gif`);
+        .setColor(colorp.red);
 
     message.channel.send(embed).then(msg => msg.delete(5000));
 }
@@ -40,8 +37,7 @@ module.exports.cantfindUser = (channel) => {
     let embed = new Discord.RichEmbed()
         .setTitle("Error")
         .setDescription("Who do you want me to ban?")
-        .setColor(colorp.red)
-        .setThumbnail(`https://i.kym-cdn.com/photos/images/original/000/787/536/32d.gif`);
+        .setColor(colorp.red);
 
     channel.send(embed).then(msg => msg.delete(5000));
 }
@@ -50,8 +46,7 @@ module.exports.noReason = (channel) => {
     let embed = new Discord.RichEmbed()
         .setTitle("Error")
         .setDescription("Why you want me to ban him?")
-        .setColor(colorp.red)
-        .setThumbnail(`https://78.media.tumblr.com/bae6877a04f06d34ef9464806c4a7296/tumblr_obwoxdilyJ1u349h5o1_500.gif`);
+        .setColor(colorp.red);
 
     channel.send(embed).then(msg => msg.delete(5000));
 }
